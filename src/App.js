fc3b2a9844462a4afd8dd6b85322fb0194eb1e39
coderdal <agircoder@gmail.com>
2022-05-28 11:51:35 +0300

    Home Page Design & Mobile compatibility

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Routes, Route } from "react-router-dom";
 
 import styles from "./App.module.css";
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { LearningIcon } from "../assets/icons";
+
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className={styles.home}>
+      <div className={styles.landingText}>
+        <h1>
+          The easiest and fastest way to learn English:
+          <br />
+          <span>Be Exposed</span>
+          <br />
+          Now it's easy with <br /> Be Exposed!
+        </h1>
+        <div className={styles.getStarted}>
+          <button>Get Started 🚀</button>
+        </div>
+      </div>
+      <div className={styles.landingFigure}>
+        <LearningIcon />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
